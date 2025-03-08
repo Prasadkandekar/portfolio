39353521f55e42b3ab9cdb9addd9e3ae5dca057f
Prasadkandekar <prasadkandekar2@gmail.com>
2025-03-08 14:08:26 +0530

    view changed

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -40,13 +40,13 @@ type TConfig = {
 
 export const config: TConfig = {
   html: {
-    title: "John Doe — 3D Portfolio",
-    fullName: "John Doe",
-    email: "johndoe@mail.com",
+    title: "Prasad's Portfolio",
+    fullName: "Prasad Kandekar",
+    email: "prasadkandekar2@gmail.com",
   },
   hero: {
-    name: "John Doe",
-    p: ["I develop 3D visuals, user", "interfaces and web applications"],
+    name: "Prasad Kandekar",
+    p: ["I'm a software developer", "specializing in web technologies."],
   },
   contact: {
     p: "Get in touch",
